dacdb918bf3d1902971fe0391c10e40a01ff350d
ddaeunbb <ddaeunbb@gmail.com>
2023-09-08 10:39:35 +0900

    Feat: 프로그래머스 바탕화면정리

--- ddaeunbb/공원산책.js
+++ ddaeunbb/공원산책.js
@@ -1,50 +1,46 @@
 function solution(park, routes) {
-    let pos = [0, 0];
-    const dict = {
-        E: [0, 1],
-        W: [0, -1],
-        S: [1, 0],
-        N: [-1, 0]
-    };
-    
-  // 시작 위치 찾기
-  for(let i = 0; i < park.length; i++) {
-      const findedIdx = park[i].indexOf('S');
-      if(findedIdx > -1) {
-          pos = [i, findedIdx];
-          break;
-      }
-  }
-  
+let pos = [0, 0];
+const dict = {
+    E: [0, 1],
+    W: [0, -1],
+    S: [1, 0],
+    N: [-1, 0]
+};
 
-  routes.forEach((route, index) => {
-      const [op, n] = route.split(' ');
-      const tempPos = [...pos];
-      let no = false;
-      
-      for(let i = 0; i < n; i++) {
-          tempPos[0] += dict[op][0];
-          tempPos[1] += dict[op][1];
-          
-          if((tempPos[0] < 0) || (tempPos[0] > park.length - 1) ||
-              (tempPos[1] < 0) || (tempPos[1] > park[0].length - 1)) {
-              no = true;
-              break;
-          }
-          
-          if(park[tempPos[0]][tempPos[1]] === 'X') {
-              no = true;
-              break;
-          }
-      }
-      
-      if(!no) {
-          pos = tempPos;
-      }
-      
-  })
-  
-  return pos;
+    // 시작 위치 찾기
+for (let i = 0; i < park.length; i++){
+    const findIdx = park[i].indexOf('S');
+    if(findIdx > -1) {
+        pos = [i, findIdx];
+        break;
+    }
+}
+
+routes.forEach((route, index) => {
+    const [op, n] = route.split(' ');
+    const tempPos = [...pos];
+    let no = false;
+    
+    for(let i = 0; i < n; i++) {
+        tempPos[0] += dict[op][0];
+        tempPos[1] += dict[op][1];
+        
+        if((tempPos[0] < 0) || (tempPos[0] > park.length - 1) ||
+        (tempPos[1] < 0) || (tempPos[1] > park[0].length - 1)) {
+            no = true;
+            break;
+        }
+
+        if(park[tempPos[0]][tempPos[1]] === 'X') {
+            no = true;
+            break;
+        }
+    }
+    if(!no) {
+        pos = tempPos;
+    }
+})
+    return pos;
 }
 
 // 처음에 풀이했던 나의 풀이..
